f94c7726b691495d9bb251b440a8b875305a69cd
m-saoud <131665242+m-saoud@users.noreply.github.com>
2023-07-08 21:20:55 +0900

    Add styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,29 @@ import { useState } from "react";
 
 import "./App.css";
 import { Container } from "react-bootstrap";
-import 'font-awesome/css/font-awesome.min.css';
-
 
 function App() {
   return (
     <>
       <h1>Build a Markdown Previewer</h1>
-      <Container className="fluid-container">
+      <container className="fluid-container">
         <div className="editor">
-
           <div class="toolbar">
-            <i class="fa fa-free-code-camp" title="no-stack-dub-sack"></i>Editor
+            <i></i>Editor
             <i class="fa fa-arrows-alt"></i>
           </div>
 
-          <textarea 
-            style={{ maxWidth: 200, maxHeight: 200 }}
-            id="editor"
-          ></textarea>
-        </div>
-{/* 
-        <div className="preview">
-          <label htmlFor="preview">Previewer:</label>
+          <textarea className="editor-textarea" id="editor"></textarea>
 
-          <textarea
-            style={{ maxWidth: 400, maxHeight: 400 }}
-            id="preview"
-          ></textarea>
-        </div> */}
-      </Container>
+          <div className="preview">
+            <div class="toolbar">
+              <i></i>Preview
+              <i class="fa fa-arrows-alt"></i>
+            </div>
+            <textarea className="editor-textarea" id="preview"></textarea>
+          </div>
+        </div>
+      </container>
     </>
   );
 }
